7f456971331e37c9852f310594887e0c178585d4
malkitsingh <malkitsingh01@gmail.com>
2017-09-04 09:04:02 +0530

    new input

--- js/custom.js
+++ js/custom.js
@@ -15,8 +15,30 @@
             $(this).parent().parent().toggleClass('active');
              $(this).parent().parent().next()
           })
-           
+            $('.modal').modal();
+           $('ul.tabs').tabs();
+    $(' .header-section  .add-more ').click(function(){
+      $('.sidebar-toggle').addClass('active');
+       $('.custom-overlay').addClass('active');
+    });
+     $('.sidebar-toggle .close').click(function(){
+      $('.sidebar-toggle').removeClass('active');
+       $('.custom-overlay').removeClass('active');
+    });
+  $('.add_comp_cat').click(function(){
+      $('.sidebar-toggle').addClass('active');
+       $('.custom-overlay').addClass('active');
+    });
+      $('.academic-head').click(function(){
+        $(this).toggleClass('active');
+        $(this).next('.academics-details-body').slideToggle();
+      });
+  $("body").niceScroll({cursorcolor:"#000",cursoropacitymax:0.7,boxzoom:true,touchbehavior:true});  // This is an IFrame (iPad compatible)
+  $(".nice-scroll").niceScroll({cursorcolor:"#0097d5",cursoropacitymax:0.7,boxzoom:true,touchbehavior:true});  // This is an IFrame (iPad compatible)
+	
+ 
   });
+
 
     $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
